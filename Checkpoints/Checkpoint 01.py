781d3ad0be3116d41e829cdf0aa1f4ee4e2d109f
fiapAluno <fiap569921@gmail.com>
2026-03-23 09:22:06 -0300

    checkpoint

--- Checkpoints/Checkpoint 01.py
+++ Checkpoints/Checkpoint 01.py
@@ -27,4 +27,23 @@ print("")
 print("="*70)
 print("Exercicio 03")
 
+nome = question('Qual é o seu nome?', "")
+valor_de_hora_de_trabalho = float(question('Quantas horas você trabalhou por dia?', ""))
+DiasTrabalho = int(question('Quantos dias você trabalhou?', ""))
 
+quantidade_de_horas_no_mes = valor_de_hora_de_trabalho * DiasTrabalho
+bonus = float(question('Qual foi o seu bonus do mes?', ""))
+desconto = float(question('Qual é o desconto total do mes?', ""))
+
+ValorBruto = valor_de_hora_de_trabalho * quantidade_de_horas_no_mes + bonus
+ValorLiquido =  ValorBruto * (desconto * 0.01)
+
+print("-"*70)
+print(f'Nome do colaborador: {nome}')
+print(f'Horas trabalhadas por dia: {valor_de_hora_de_trabalho:.0f} horas')
+print(f'Horas trabalhadas no mês: {quantidade_de_horas_no_mes:.0f} horas')
+print(f'Bônus: R${bonus:.2f}')
+print(f'Desconto: R${desconto:.2f} ({desconto:.0f}%)')
+print(f'Seu salario bruto: R${ValorBruto:.2f}')
+print(f'Seu salario liquido: R${ValorLiquido:.2f}')
+print("-"*70)
